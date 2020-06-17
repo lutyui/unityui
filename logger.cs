fda1d796efb35f982f4ca61c52f08b7fc217e439
lutyui <luty@luty.info>
2020-06-17 17:21:50 +0900

    Add loggerCSV

--- logger.cs
+++ logger.cs
@@ -7,20 +7,29 @@ using System.Linq;
 public class logger : MonoBehaviour
 {
     utils3D util3D = new utils3D();
+    loggerCSV position_logger;
+    loggerCSV position_logger2;
     public GameObject[] cubes = new GameObject[3];
     public GameObject src;
     public GameObject dst;
     public GameObject target;
     // Start is called before the first frame update
     private LineRenderer line;
+    void OnEnable()
+    {
+        position_logger = this.GetComponents<loggerCSV>()[0];
+        position_logger2 = this.GetComponents<loggerCSV>()[1];
+    }
     void Start()
     {
         util3D.hello_world();
 
-        float[] array = {3f, 1f, 4f};
-        line = util3D.draw_line(src, dst, length:10f, radius:0.001f);
+        float[] array = { 3f, 1f, 4f };
+        line = util3D.draw_line(src, dst, length: 10f, radius: 0.001f);
         line.material = new Material(Shader.Find("Mobile/Particles/Additive"));
         //line.material = new Material(Shader.Find("Mobile/Particles/Additive"));
+        var a = position_logger.write_line("x,y,x");
+        var b = position_logger2.write_line("ax,ay,ax");
 
     }
     // Update is called once per frame
@@ -32,8 +41,8 @@ public class logger : MonoBehaviour
         plane_equation = util3D.calc_plane_from_3objects_position(cubes[0].transform.position, cubes[1].transform.position, cubes[2].transform.position);
         Debug.Log(util3D.distance_from_plane(plane_equation, target.transform.position));
         */
-        util3D.update_line(line, src, dst, length:10f);
-        Debug.Log(util3D.distance_from_line(src.transform.position , dst.transform.position, target.transform.position));
+        util3D.update_line(line, src, dst, length: 10f);
+        Debug.Log(util3D.distance_from_line(src.transform.position, dst.transform.position, target.transform.position));
         if (Input.GetKey(KeyCode.C))
         {
             line.startColor = Color.blue;
@@ -41,6 +50,13 @@ public class logger : MonoBehaviour
 
             Debug.Log("color changed");
         }
+        float[] array = new float[3];
+        for (int i = 0; i < 3; i++)
+        {
+            array[i] = target.transform.position[i];
+        }
+        var a = position_logger.write_line_float(array);
+        var b = position_logger2.write_line_float(array);
 
     }
 }
---
+++ loggerCSV.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System;
+
+public class loggerCSV : MonoBehaviour
+{
+    [SerializeField]
+    private string dirname = "";
+    [SerializeField]
+    private string filename = "log";
+    private StreamWriter stream_writer;
+    void OnEnable()
+    {
+        if (dirname == "")
+        {
+            dirname = "Assets/Data/CSV/";
+        }
+        if (!Directory.Exists(dirname))
+        {
+            Directory.CreateDirectory(dirname);
+        }
+        var file_full_path = dirname + filename + ".csv";
+        int file_index = 0;
+        while (true)
+        {
+            if (!File.Exists(file_full_path))
+            {
+                stream_writer = new StreamWriter(file_full_path, append: true);
+                break;
+            }
+            else
+            {
+                file_full_path = dirname + filename + file_index.ToString() + ".csv";
+                file_index += 1;
+            }
+        }
+    }
+
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int write_line(string str)
+    {
+        stream_writer.WriteLine(str);
+        return 0;
+    }
+
+    public int write_line_string(string[] array)
+    {
+        string log_string = String.Join(",", array);
+        stream_writer.WriteLine(log_string);
+        return 0;
+    }
+    public int write_line_float(float[] array)
+    {
+        int length = array.Length;
+        string[] str = new string[length];
+        for (int i = 0; i < length; i++)
+        {
+            str[i] = array[i].ToString();
+        }
+        string log_string = String.Join(",", array);
+        stream_writer.WriteLine(log_string);
+        return 0;
+    }
+    void OnApplicationQuit()
+    {
+        stream_writer.Flush();
+        stream_writer.Close();
+    }
+}
